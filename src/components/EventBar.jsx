f6f1abce466c0610497357844e9e0c87ea5bf036
韩毅 <2387870957@qq.com>
2023-01-14 22:32:59 +0800

    feat: 可以选择事件

--- src/components/EventBar.jsx
+++ src/components/EventBar.jsx
@@ -1,10 +1,10 @@
 import AddButton from "./AddButton";
 
-const EventBar = ({ events, setEvents }) => {
+const EventBar = ({ events, setEvents, currentEvent, setCurrentEvent }) => {
   const handleAdd = () => {
     const title = prompt("请输入事件名：");
     if (title) {
-      setEvents((prev) => [...prev, { title: title }]);
+      setEvents((prev) => [...prev, { title: title, tasks: {} }]);
     }
   };
 
@@ -17,7 +17,13 @@ const EventBar = ({ events, setEvents }) => {
       <div className="event-container">
         <AddButton handleClick={handleAdd} />
         {events.map((item) => (
-          <div key={item.title} className="event over-hide">
+          <div
+            key={item.title}
+            className={`event over-hide ${
+              currentEvent.title === item.title ? "selected-event" : null
+            }`}
+            onClick={() => setCurrentEvent(item)}
+          >
             {item.title}
           </div>
         ))}
