28d7632ecdc549ba85368fbf3e9edbfc29c5b03b
韩毅 <2387870957@qq.com>
2023-01-18 14:14:35 +0800

    fix: initEvent

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,22 @@ import EventBar from "./components/EventBar";
 import TaskBox from "./components/TaskBox";
 
 function App() {
+  const initEvent = [
+    {
+      title: "添加一项新事件",
+      ["To Do"]: [],
+      ["In Progress"]: [],
+      ["Completed"]: [],
+    },
+  ];
   const [events, setEvents] = React.useState(() => {
-    return localStorage.getItem("events")
-      ? JSON.parse(localStorage.getItem("events"))
-      : [
-          {
-            title: "添加一项新事件",
-            ["To Do"]: [],
-            ["In Progress"]: [],
-            ["Completed"]: [],
-          },
-        ];
+    return localStorage.getItem("events") ? JSON.parse(localStorage.getItem("events")) : initEvent;
   });
   const [currentEvent, setCurrentEvent] = React.useState(events[0]);
 
   React.useEffect(() => {
     if (!events.length) {
-      localStorage.setItem(
-        "events",
-        JSON.stringify([
-          {
-            title: "添加一项新事件",
-            ["To Do"]: [],
-            ["In Progress"]: [],
-            ["Completed"]: [],
-          },
-        ])
-      );
+      localStorage.setItem("events", JSON.stringify(initEvent));
       setEvents(JSON.parse(localStorage.getItem("events")));
     } else {
       localStorage.setItem("events", JSON.stringify(events));
