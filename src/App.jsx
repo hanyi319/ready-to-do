c09fa2ba25592e160cca2e1b19dff84cf91b5fed
韩毅 <2387870957@qq.com>
2023-01-18 10:54:08 +0800

    feat: 使用 localStorage 存储数据

--- src/App.jsx
+++ src/App.jsx
@@ -4,84 +4,40 @@ import "./components/event.css";
 import "./components/task.css";
 import EventBar from "./components/EventBar";
 import TaskBox from "./components/TaskBox";
-import uuid from "react-uuid";
 
 function App() {
-  const [events, setEvents] = React.useState([
-    {
-      title: "工作",
-      ["To Do"]: [
-        {
-          name: "工作任务 1",
-          id: uuid(),
-          details: "这是一段测试用的文本。",
-        },
-      ],
-      ["In Progress"]: [
-        {
-          name: "工作任务 2",
-          id: uuid(),
-          details: "这是一段测试用的文本。",
-        },
-      ],
-      ["Completed"]: [
-        {
-          name: "工作任务 3",
-          id: uuid(),
-          details: "这是一段测试用的文本。",
-        },
-      ],
-    },
-    {
-      title: "学习",
-      ["To Do"]: [
-        {
-          name: "学习任务 1",
-          id: uuid(),
-          details: "这是一段测试用的文本。",
-        },
-      ],
-      ["In Progress"]: [
-        {
-          name: "学习任务 2",
-          id: uuid(),
-          details: "这是一段测试用的文本。",
-        },
-      ],
-      ["Completed"]: [
-        {
-          name: "学习任务 3",
-          id: uuid(),
-          details: "这是一段测试用的文本。",
-        },
-      ],
-    },
-    {
-      title: "健身",
-      ["To Do"]: [
-        {
-          name: "健身任务 1",
-          id: uuid(),
-          details: "这是一段测试用的文本。",
-        },
-      ],
-      ["In Progress"]: [
-        {
-          name: "健身任务 2",
-          id: uuid(),
-          details: "这是一段测试用的文本。",
-        },
-      ],
-      ["Completed"]: [
-        {
-          name: "健身任务 3",
-          id: uuid(),
-          details: "这是一段测试用的文本。",
-        },
-      ],
-    },
-  ]);
+  const [events, setEvents] = React.useState(() => {
+    return localStorage.getItem("events")
+      ? JSON.parse(localStorage.getItem("events"))
+      : [
+          {
+            title: "添加一项新事件",
+            ["To Do"]: [],
+            ["In Progress"]: [],
+            ["Completed"]: [],
+          },
+        ];
+  });
   const [currentEvent, setCurrentEvent] = React.useState(events[0]);
+
+  React.useEffect(() => {
+    if (!events.length) {
+      localStorage.setItem(
+        "events",
+        JSON.stringify([
+          {
+            title: "添加一项新事件",
+            ["To Do"]: [],
+            ["In Progress"]: [],
+            ["Completed"]: [],
+          },
+        ])
+      );
+      setEvents(JSON.parse(localStorage.getItem("events")));
+    } else {
+      localStorage.setItem("events", JSON.stringify(events));
+    }
+  }, [events]);
 
   return (
     <div className="App">
